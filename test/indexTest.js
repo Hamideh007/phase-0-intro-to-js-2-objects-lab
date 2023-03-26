35bc4f75e23222901edc27d21b494d831a40edc1
hamideh rahmani <rahmani.mahtab@gmail.com>
2023-03-26 19:02:17 -0400

    complete object lab

--- test/indexTest.js
+++ test/indexTest.js
@@ -68,3 +68,24 @@ describe('employees', function() {
     });
   });
 });
+const employee = {
+  name: 'John Smith',
+  streetAddress: '123 Main St'
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return { ...employee, [key]: value };
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = { ...employee };
+  delete newEmployee[key];
+  return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
+
